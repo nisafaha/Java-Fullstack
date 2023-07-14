f5dc1499047121bfd04a6e8d30999fd6603a904b
Nisaf Ahamed <87429268+nisafaha@users.noreply.github.com>
2023-07-14 22:57:31 +0530

    mysql database connected

---
+++ fullstack-backend/src/main/java/com/codewithnisaf/fullstackbackend/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.codewithnisaf.fullstackbackend.controller;
+import com.codewithnisaf.fullstackbackend.model.User;
+import com.codewithnisaf.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+}
