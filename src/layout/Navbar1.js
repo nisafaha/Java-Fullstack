d208d47b44fc8c8b4529e96a44f4e40918305ee3
Nisaf Ahamed <87429268+nisafaha@users.noreply.github.com>
2023-07-18 22:15:31 +0530

    Update Nav

--- src/layout/Navbar1.js
+++ src/layout/Navbar1.js
@@ -4,17 +4,17 @@ export default function Navbar() {
   return (
     <div>
 
-<nav className="navbar navbar-expand-lg navbar-dark bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#" style={{color: 'Red'}}>Roam SriLanka</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#" style={{ color: 'Red' }}>Roam SriLanka Nav</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-    
-    
-    </div>
-</nav>
+
+
+        </div>
+      </nav>
     </div>
   )
 }
